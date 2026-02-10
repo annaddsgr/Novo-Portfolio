042e97c7b97036af8c4428fbab2eb631cf2bc413
Anna Portfolio <contact@anna.com>
2026-02-10 14:43:29 -0300

    Configure multi-env build (gh-pages and Vercel)

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Novo-Portfolio/' : '/',
+  base: process.env.BASE_PATH || '/',
   plugins: [
     react(),
     tailwindcss(),
